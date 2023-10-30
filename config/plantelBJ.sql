b2076502d9a88b04d41a58a010adaafdb958212b
Axel Daniel <axeelleguero@gmail.com>
2023-10-29 23:13:34 -0300

    Plantel primeros 11 agregados a BD

--- config/plantelBJ.sql
+++ config/plantelBJ.sql
@@ -2,9 +2,13 @@ create database plantel;
  use plantel;
 
  CREATE TABLE jugador (
-    codigo INT(10) PRIMARY KEY NOT NULL,
+    codigo INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre VARCHAR(12) NOT NULL,
     apellido VARCHAR(20) NOT NULL,
     foto VARCHAR(50) NOT NULL,
+    posicion varchar(3) NOT NULL,
     dorsal INT(25) NOT NULL
 );
+
+INSERT INTO jugador(nombre, apellido, posicion, dorsal) VALUES("Sergio", "Romero", "Arq", 1), ("Facundo", "Roncaglia", "DFC", 2), ("Marcelo", "Saracchi", "DFI", 3), ("Nicolas", "Figal", "DFC", 4), ("Ezequiel", "Bullaude", "VC", 5),
+("Marcos", "Rojo", "DFC", 6), ("Ezequiel", "Zeballos", "DE", 7), ("Guillermo", "Fernandez", "VD", 8),("Dario", "Benedetto", "DE", 9), ("Edinson", "Cavani", "DE", 10), ("Lucas", "Janson", "DE", 11);
