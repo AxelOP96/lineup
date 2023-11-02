b7d9ebc31bcaa5e78fe615453ea13be51af29b44
Axel Daniel <axeelleguero@gmail.com>
2023-11-02 00:03:49 -0300

    Mejora de la vista y la BD

--- config/plantelBJ.sql
+++ config/plantelBJ.sql
@@ -12,7 +12,7 @@ create database plantel;
 
 INSERT INTO jugador(nombre, apellido, posicion, dorsal) VALUES("Sergio", "Romero", "ARQ", 1), ("Facundo", "Roncaglia", "DF", 2), ("Marcelo", "Saracchi", "DF", 3), ("Nicolas", "Figal", "DF", 4), ("Ezequiel", "Bullaude", "MC", 5),
 ("Marcos", "Rojo", "DF", 6), ("Ezequiel", "Zeballos", "DE", 7), ("Guillermo", "Fernandez", "MC", 8),("Dario", "Benedetto", "DE", 9), ("Edinson", "Cavani", "DE", 10), ("Lucas", "Janson", "DE", 11);
-INSERT INTO jugador(nombre, apellido, posicion, dorsal) VALUES("Leandro", "Brey", "ARQ", 12), ("Javier", "Garcia", "ARQ", 13), ("Nicolas", "Valentini", "DFC", 15), ("Miguel", "Merentiel", "DE", 16), ("Luis", "Advincula", "DFD", 17),
+INSERT INTO jugador(nombre, apellido, posicion, dorsal) VALUES("Leandro", "Brey", "ARQ", 12), ("Javier", "Garcia", "ARQ", 13), ("Nicolas", "Valentini", "DF", 15), ("Miguel", "Merentiel", "DE", 16), ("Luis", "Advincula", "DF", 17),
 ("Frank", "Fabra", "DF", 18), ("Valentin", "Barco", "MC", 19), ("Juan", "Ramirez", "MC", 20),("Ezequiel", "Fernandez", "MC", 21), ("Diego", "Gonzalez", "MC", 23), ("Bruno", "Valdez", "DF", 25);
-INSERT INTO jugador(nombre, apellido, posicion, dorsal) VALUES("Norberto", "Briasco", "DE", 29), ("Nahuel", "Genez", "DFI", 35), ("Cristian", "Medina", "VC", 36), ("Aaron", "Anselmino", "DFC", 38), ("Vicente", "Taborda", "VC", 39),
+INSERT INTO jugador(nombre, apellido, posicion, dorsal) VALUES("Norberto", "Briasco", "DE", 29), ("Nahuel", "Genez", "DF", 35), ("Cristian", "Medina", "MC", 36), ("Aaron", "Anselmino", "DFC", 38), ("Vicente", "Taborda", "MC", 39),
 ("Lautaro", "Di Lollo", "DF", 40), ("Luca", "Langoni", "DE", 41), ("Lucas", "Blondel", "DF", 42),("Jorman", "Campuzano", "MC", 49), ("Marcelo", "Weigandt", "DF", 57);
