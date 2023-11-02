ad293cd25cd33980c394248a0a54deb891b7ae4e
Axel Daniel <axeelleguero@gmail.com>
2023-11-02 14:44:57 -0300

    Modificacion logica de controller

--- controller/InicioController.php
+++ controller/InicioController.php
@@ -9,10 +9,41 @@ class InicioController
         $this->render = $render;
         $this->model = $model;
     }
+    public function procesarFormulario(){
+        $datos = null;
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $jugador1 = $_POST["jugador1"];
+            $jugador2 = $_POST["jugador2"];
+            $jugador3 = $_POST["jugador3"];
+            $jugador4 = $_POST["jugador4"];
+            $jugador5 = $_POST["jugador5"];
+            $jugador6 = $_POST["jugador6"];
+            $jugador7 = $_POST["jugador7"];
+            $jugador8 = $_POST["jugador8"];
+            $jugador9 = $_POST["jugador9"];
+            $jugador10 = $_POST["jugador10"];
+            $jugador11 = $_POST["jugador11"];
+        
+            $jugadores = array();
+
+            for ($i = 1; $i <= 11; $i++) {
+            $jugador = "jugador" . $i;
+            if (isset($_POST['$jugador'])) {
+                $jugadores[] = $_POST['$jugador'];
+            }
+        }
+        var_dump($jugador);
+        $datos['jugadores'] = $jugadores;
+        var_dump($datos['jugadores']);
+    }
+       
+        return $datos;
+        }
+        
 
     public function mostrarPantallaInicial()
     {
-        $datos = null;
+        $datos['jugadores'] =  $this->procesarFormulario();//$this->model->getDelanteros();
         $this->render->printView('index', $datos);
     }
 }
