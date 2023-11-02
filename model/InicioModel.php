06b8c4f75f9361f7c6e84261bca7a58874939268
Axel Daniel <axeelleguero@gmail.com>
2023-11-02 01:08:23 -0300

    Funciones sql y select en form

--- model/InicioModel.php
+++ model/InicioModel.php
@@ -10,4 +10,21 @@ class InicioModel {
     public function getDatabase(){
         return $this->database;
     }
+
+    public function getDelanteros($nombre, $posicion){
+        $query = "SELECT * FROM jugador WHERE nombre LIKE '%$nombre%' and posicion LIKE '%$posicion%'";
+        return $this->database->query( $query );
+    }
+    public function getMediocampistas($nombre, $posicion){
+        $query = "SELECT * FROM jugador WHERE nombre LIKE '%$nombre%' and posicion LIKE '%$posicion%'";
+        return $this->database->query( $query );
+    }
+    public function getDefensores($nombre, $posicion){
+        $query = "SELECT * FROM jugador WHERE nombre LIKE '%$nombre%' and posicion LIKE '%$posicion%'";
+        return $this->database->query( $query );
+    }
+    public function getArquero($nombre, $posicion){
+        $query = "SELECT * FROM jugador WHERE nombre LIKE '%$nombre%' and posicion LIKE '%$posicion%'";
+        return $this->database->query( $query );
+    }
 }
